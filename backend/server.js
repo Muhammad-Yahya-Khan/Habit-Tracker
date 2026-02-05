44bf10f0708d521e57717493b5ecb097d8359760
Muhammad Yahya Khan <Yahyakhanazmi2003@gmail.com>
2026-02-05 09:31:09 +0500

    Completed

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://habit-tracker-livid-zeta.vercel.app",
+];
 
 const corsOptions = {
     origin: function (origin, callback) {
