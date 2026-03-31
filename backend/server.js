e32aab07df0e65abdadea843ae595d209b4fea8d
Muhammad Yahya Khan <163578387+Muhammad-Yahya-Khan@users.noreply.github.com>
2026-03-31 23:27:23 +0500

    Update CORS middleware configuration

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: "https://habit-tracker-livid-zeta.vercel.app/ " || "*",
+    }),
+);
 app.use(express.json());
 
 // MongoDB Connection
